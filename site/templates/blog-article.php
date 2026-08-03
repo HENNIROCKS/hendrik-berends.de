6d0df840c783ddf1908751c4fd2b9c70578b2425
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2026-08-03 19:55:24 +0200

    Remove Mastodon replies from frontend

--- site/templates/blog-article.php
+++ site/templates/blog-article.php
@@ -26,8 +26,6 @@ use Kirby\Toolkit\Str;
         <?php snippet('tags/tags') ?>
 
         <?php snippet('layouts', ['layout_src' => $page->layouts()]) ?>
-
-        <?php snippet('mastodon-replies') ?>
     </section>
 </main>
 
