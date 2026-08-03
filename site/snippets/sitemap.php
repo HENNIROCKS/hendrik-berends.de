cb4e2b2d12ec95a8d063f41dd1575c420cd8aa3f
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2026-08-03 11:26:17 +0200

    Fix sitemap

--- site/snippets/sitemap.php
+++ site/snippets/sitemap.php
@@ -7,9 +7,7 @@
  * https://getkirby.com/docs/cookbook/navigation/sitemap
  */
 
-?>
-
-<?= '<?xml version="1.0" encoding="utf-8"?>' ?>
+?><?= '<?xml version="1.0" encoding="utf-8"?>' ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($pages as $p): ?>
